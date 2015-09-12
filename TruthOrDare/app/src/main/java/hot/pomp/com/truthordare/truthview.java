8918023c360d5ce24fa93094c90dca33f9988365
patrickeschbach <pjeschba@umich.edu>
2015-09-12 00:29:24 -0400

    :O

---
+++ TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/truthview.java
@@ -0,0 +1,11 @@
+package hot.pomp.com.truthordare;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by patrickeschbach on 9/12/15.
+ */
+public class truthview extends AppCompatActivity {
+
+
+}
