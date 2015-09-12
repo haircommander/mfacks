e20a37e7c360317db1bac6859681de94eaf69701
Owen <orodriguez49@gmail.com>
2015-09-12 03:12:43 -0400

    Working on TextAssets.java, a class to handle text resources.

--- TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/TextAssets.java
+++ TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/TextAssets.java
@@ -1,5 +1,7 @@
 package hot.pomp.com.truthordare;
 
+import android.content.Context;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,17 +9,21 @@ import java.util.List;
 /**
  * Created by Owen on 9/12/15.
  */
-public abstract class TextAssets {
+public class TextAssets {
     public static List<String> TRUTHS;
 
     private static File truthsFile;
+    private static Context context;
 
-    public static void initialize() {
+    private static String TRUTHS_FILENAME = "truths.txt";
+
+    public  void initialize(Context context) {
         TRUTHS = new ArrayList<String>();
+        this.context = context;
         load();
     }
 
     private static void load() {
-        //truthsFile
+        truthsFile = new File(context.getFilesDir(), TRUTHS_FILENAME);
     }
 }
