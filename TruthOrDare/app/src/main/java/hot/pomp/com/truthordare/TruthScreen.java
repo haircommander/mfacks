11ffec10de9314492de506e3ba29bd61cf9c2d72
patrickeschbach <pjeschba@umich.edu>
2015-09-12 19:37:48 -0400

    Fixed keep playing button

--- TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/TruthScreen.java
+++ TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/TruthScreen.java
@@ -19,5 +19,15 @@ public class TruthScreen extends AppCompatActivity {
         ((TextView)findViewById(R.id.truthTextView)).setText(getIntent().getStringExtra(TruthSelect.TRUTH));
     }
 
+    public void openMainMenu(View view)
+    {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
 
+    public void openExitScreen(View view)
+    {
+        Intent intent = new Intent(this, ExitScreen.class);
+        startActivity(intent);
+    }
 }
