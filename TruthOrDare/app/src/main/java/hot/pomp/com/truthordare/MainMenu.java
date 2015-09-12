fdcb27bc47a2074e68e7ed7acdd707f8bf25ad7b
patrickeschbach <pjeschba@umich.edu>
2015-09-12 16:01:17 -0400

    Truth Screen Background

--- TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/MainMenu.java
+++ TruthOrDare/app/src/main/java/hot/pomp/com/truthordare/MainMenu.java
@@ -1,11 +1,14 @@
 package hot.pomp.com.truthordare;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 
 import java.util.Random;
 
